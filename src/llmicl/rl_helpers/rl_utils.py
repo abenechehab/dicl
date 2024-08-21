eb4a796cca183275aa662e6fa208488c46abb5a9
Abdelhakim Benechehab <abdelhakim.benechehab@gmail.com>
2024-08-21 09:22:55 +0000

    ✨ add legacy calculate_multiPDF

--- src/llmicl/rl_helpers/rl_utils.py
+++ src/llmicl/rl_helpers/rl_utils.py
@@ -16,6 +16,7 @@ import gymnasium as gym
 from dm_control import suite
 
 from llmicl.legacy.data.serialize import serialize_arr, SerializerSettings
+from llmicl.legacy.models.ICL import recursive_refiner, trim_kv_cache
 
 if TYPE_CHECKING:
     from llmicl.interfaces.trainers import ICLObject
@@ -91,6 +92,79 @@ def calculate_multiPDF_llama3(
     # release memory
     del logit_mat, kv_cache_prev  #, kv_cache_main
     return PDF_list, probs, kv_cache_main
+
+def calculate_multiPDF(
+    full_series, model, tokenizer, prec, mode="neighbor", refine_depth=1
+):
+    """
+    This function calculates the multi-resolution probability density function (PDF) for a given series.
+
+    Parameters:
+    full_series (str): The series for which the PDF is to be calculated.
+    prec (int): The precision of the PDF.
+    mode (str, optional): The mode of calculation. Defaults to 'neighbor'.
+    refine_depth (int, optional): The depth of refinement for the PDF. Defaults to 1.
+    llama_size (str, optional): The size of the llama model. Defaults to '13b'.
+
+    Returns:
+    list: A list of PDFs for the series.
+    """
+    good_tokens_str = list("0123456789")
+    good_tokens = [tokenizer.convert_tokens_to_ids(token) for token in good_tokens_str]
+    assert refine_depth < prec, "Refine depth must be less than precision"
+    refine_depth = refine_depth - prec
+    curr = -prec
+    batch = tokenizer([full_series], return_tensors="pt", add_special_tokens=True)
+    torch.cuda.empty_cache()
+    with torch.no_grad():
+        out = model(batch['input_ids'].cuda(), use_cache=True)
+        # out = model(batch["input_ids"].cpu(), use_cache=True)
+    logit_mat = out["logits"]
+    kv_cache_main = out["past_key_values"]
+    logit_mat_good = logit_mat[:, :, good_tokens].clone()
+    probs = torch.nn.functional.softmax(logit_mat_good[:, 1:, :], dim=-1)
+
+    PDF_list = []
+    comma_locations = np.sort(np.where(np.array(list(full_series)) == ",")[0])
+
+    for i in range(len(comma_locations)):
+        PDF = MultiResolutionPDF()
+        # slice out the number before ith comma
+        if i == 0:
+            start_idx = 0
+        else:
+            start_idx = comma_locations[i - 1] + 1
+        end_idx = comma_locations[i]
+        num_slice = full_series[start_idx:end_idx]
+        prob_slice = probs[0, start_idx:end_idx].cpu().numpy()
+        ### Load hierarchical PDF
+        PDF.load_from_num_prob(num_slice, prob_slice)
+
+        ### Refine hierarchical PDF
+        seq = full_series[:end_idx]
+        # cache and full_series are shifted from beginning, not end
+        end_idx_neg = end_idx - len(full_series)
+        ### kv cache contains seq[0:-1]
+        kv_cache = trim_kv_cache(kv_cache_main, end_idx_neg - 1)
+        recursive_refiner(
+            PDF,
+            seq,
+            curr=curr,
+            main=True,
+            refine_depth=refine_depth,
+            mode=mode,
+            kv_cache=kv_cache,
+            model=model,
+            tokenizer=tokenizer,
+            good_tokens=good_tokens,
+        )
+
+        PDF_list += [PDF]
+
+    # release memory
+    del logit_mat, kv_cache_main
+    return PDF_list
+
 
 def serialize_gaussian(prec, time_series, mean_series, sigma_series):
     """
